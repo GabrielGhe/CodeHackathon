4511ce955acd25aa8d37bc42b169201ab856465f
reetah <rita.lu410@gmail.com>
2014-03-01 11:46:15 -0500

    google totally working now

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -2,9 +2,10 @@
 
 $(document).ready(function(){
 // google maps api
-
+  var geocoder;
   var map;
   function initialize() {
+    geocoder = new google.maps.Geocoder();
     var mapOptions = {
       zoom: 3,
       center: new google.maps.LatLng(45.3, -73.34)
@@ -39,29 +40,22 @@ $(document).ready(function(){
   };
 
   make_marker(45.3, -73.34);
-  make_marker(45.3, -45.00);
 
-  // var marker1 = new google.maps.Marker({
-  //   position: new google.maps.LatLng(45.3, -73.34),
-  //   map: map,
-  //   title:"Hello World!"
-  // });
+// this function will output a marker for the city directly on the map
+  function codeAddress(city) {
+    var address = city;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
 
-
-function codeAddress() {
-  var address = document.getElementById('address').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+   codeAddress('Vancouver');
 });
-
-// window.onload = loadScript;
