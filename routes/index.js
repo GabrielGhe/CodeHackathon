4f0dac794260dbb40e985ad9b1301f4d369c15a6
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-02-28 22:15:01 -0500

    Made json do stuff

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,35 @@ var fs = require('fs');
  * GET home page.
  */
 exports.index = function(req, res){
+	/*
 	var newJSON = [];
+	var fname = "./data/newRent.json";
+
 	for(var i = 0; i < rentJSON.length; i++){
 		if(rentJSON[i].Ref_Date == 2013){
 			newJSON.push(rentJSON[i]);
-			console.log(rentJSON[i]);
 		}
 	}
-	var outfile = "../data/newRent.json";
-	fs.writeFile(outfile, JSON.stringify(newJSON), function(err) {
+	
+	fs.writeFile(fname, newJSON, function(err) {
 	    if(err) {
 	      console.log(err);
 	    } else {
-	      console.log("JSON saved to " + outfile);
+	      console.log("JSON saved to ");
 	    }
 	});
+	*/
+	for(var i = 0; i < rentJSON.length; i++){
+		console.log(rentJSON[i]);
+		console.log("");
+		console.log("");
+	}
+	
 	//city.enterAll();
+	
+	//render
+	res.render('index', {
+		title: 'Awesome App'
+	});
 };
 
